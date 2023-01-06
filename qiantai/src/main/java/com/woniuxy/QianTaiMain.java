e2d80dc5e739d47affbd6431d1169c3054a9b1a5
woniumrwang <woniumrwang@qq.com>
2023-01-06 14:18:59 +0800

    QianTai基本环境准备

---
+++ qiantai/src/main/java/com/woniuxy/QianTaiMain.java
@@ -0,0 +1,12 @@
+package com.woniuxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QianTaiMain {
+    public static void main(String[] args) {
+        SpringApplication.run(QianTaiMain.class);
+    }
+
+}
