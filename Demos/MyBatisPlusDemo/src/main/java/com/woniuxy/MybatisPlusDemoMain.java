fcc17b5c6f865a785d860c72b14670b33137f3f1
woniumrwang <woniumrwang@qq.com>
2023-01-06 14:45:55 +0800

    MybatisPlus逆向生成代码

---
+++ Demos/MyBatisPlusDemo/src/main/java/com/woniuxy/MybatisPlusDemoMain.java
@@ -0,0 +1,12 @@
+package com.woniuxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusDemoMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusDemoMain.class);
+    }
+
+}
