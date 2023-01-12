17ca099e9b5df783f46478407f54cbefeb21edc8
woniumrwang <woniumrwang@qq.com>
2023-01-12 17:38:54 +0800

    登录页实现验证码

--- qiantai/src/main/java/com/woniuxy/qiantai/controller/UserController.java
+++ qiantai/src/main/java/com/woniuxy/qiantai/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.woniuxy.qiantai.controller;
 
 
+import com.google.code.kaptcha.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * <p>
@@ -16,6 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    Producer producer;
+
+    @RequestMapping("getKaptchaImage")
+    public void getKaptchaImage(HttpServletResponse response) throws IOException {
+        //生成验证码
+        String codeText = producer.createText();
+        BufferedImage codeImage = producer.createImage(codeText);
+
+        response.setContentType("image/png");
+        ImageIO.write(codeImage,"png",response.getOutputStream());
+
+    }
+
+
 
 }
 
