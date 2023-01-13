8571e6e444843f11400dcd13f31450bb459e8d02
woniumrwang <woniumrwang@qq.com>
2023-01-13 11:55:46 +0800

    扩展线程的异步执行

--- Demos/RabbitMqDemo/src/main/java/com/woniuxy/Main.java
+++ Demos/RabbitMqDemo/src/main/java/com/woniuxy/Main.java
@@ -1,7 +1,35 @@
 package com.woniuxy;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println(Thread.currentThread().getName());
+
+        //开启一个新线程,异步执行任务
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //新线程中执行任务
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        System.out.println(Thread.currentThread().getName() + i);
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        },"subThread").start();
+
+        //主线程里的for循环
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+
     }
 }
