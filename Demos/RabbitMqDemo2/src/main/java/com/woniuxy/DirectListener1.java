66ff0424fc8e9af59f961d4ce6c0865515ba6718
woniumrwang <woniumrwang@qq.com>
2023-01-14 10:34:25 +0800

    boot下自动ack的重试机制

--- Demos/RabbitMqDemo2/src/main/java/com/woniuxy/DirectListener1.java
+++ Demos/RabbitMqDemo2/src/main/java/com/woniuxy/DirectListener1.java
@@ -12,12 +12,27 @@ public class DirectListener1 {
     @RabbitListener(queues = "TestDirectQueue1")
 //    @RabbitHandler
     public void onMsg(Map<String,Object> map){
-        System.out.println("DirectListener1 onMsg");
-        Set<String> keys = map.keySet();
-        keys.forEach(key->{
-            System.out.println(key);
-            System.out.println(map.get(key));
-        });
+
+        //自动ack的时候,如果方法正常执行,就确认消息被消费
+        //如果方法执行失败,则自动重试
+        try {
+            //int a = 3 / 0;
+            System.out.println("执行业务逻辑......");
+
+            System.out.println("DirectListener1 onMsg");
+            Set<String> keys = map.keySet();
+            keys.forEach(key->{
+                System.out.println(key);
+                System.out.println(map.get(key));
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+            //log.....
+            //自动ack的时候,监听方法里不要抛出异常,否则会无限重试,导致死循环
+        }
+
+
+
     }
 
 }
