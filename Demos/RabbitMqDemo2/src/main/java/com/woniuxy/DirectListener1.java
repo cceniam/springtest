4adedcf19bb531fd39965f3140e8b3d37bfd0aba
woniumrwang <woniumrwang@qq.com>
2023-01-14 10:53:08 +0800

    boot下手动ack

--- Demos/RabbitMqDemo2/src/main/java/com/woniuxy/DirectListener1.java
+++ Demos/RabbitMqDemo2/src/main/java/com/woniuxy/DirectListener1.java
@@ -1,8 +1,11 @@
 package com.woniuxy;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,12 +14,12 @@ public class DirectListener1 {
 
     @RabbitListener(queues = "TestDirectQueue1")
 //    @RabbitHandler
-    public void onMsg(Map<String,Object> map){
+    public void onMsg(Map<String,Object> map, Channel channel, Message message) throws IOException {
 
         //自动ack的时候,如果方法正常执行,就确认消息被消费
         //如果方法执行失败,则自动重试
         try {
-            //int a = 3 / 0;
+            int a = 3 / 0;
             System.out.println("执行业务逻辑......");
 
             System.out.println("DirectListener1 onMsg");
@@ -25,10 +28,16 @@ public class DirectListener1 {
                 System.out.println(key);
                 System.out.println(map.get(key));
             });
+
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
         }catch (Exception e){
+            //todo 有需要完成的任务
+            //fixme 没有拒绝消息的效果
             e.printStackTrace();
             //log.....
             //自动ack的时候,监听方法里不要抛出异常,否则会无限重试,导致死循环
+            //channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
+            channel.basicReject(message.getMessageProperties().getDeliveryTag(),false);
         }
 
 
--- Demos/RabbitMqDemo2/src/main/java/com/woniuxy/DirectListener2.java
+++ Demos/RabbitMqDemo2/src/main/java/com/woniuxy/DirectListener2.java
@@ -1,8 +1,11 @@
 package com.woniuxy;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,13 +14,18 @@ public class DirectListener2 {
 
     @RabbitListener(queues = "TestDirectQueue2")
 //    @RabbitHandler
-    public void onMsg(Map<String,Object> map){
+    public void onMsg(Map<String,Object> map, Channel channel, Message message) throws IOException {
         System.out.println("DirectListener2 onMsg");
         Set<String> keys = map.keySet();
         keys.forEach(key->{
             System.out.println(key);
             System.out.println(map.get(key));
         });
+
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+
+        //channel.basicReject();
+
     }
 
 }
