05f7acefd43c3dc75873c93add32997f0079b34d
woniumrwang <woniumrwang@qq.com>
2023-01-14 10:13:25 +0800

    准备RabbitMqDemo2

---
+++ Demos/RabbitMqDemo2/src/main/java/com/woniuxy/DirectController.java
@@ -0,0 +1,49 @@
+package com.woniuxy;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class DirectController {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @RequestMapping("/send")
+    @ResponseBody
+    public String send(String exchange, String routingKey, String name){
+
+        //准备发送的数据
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "test message, hello!";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+        map.put("name",name);
+
+        rabbitTemplate.convertAndSend(exchange,routingKey,map);
+
+        return "ok";
+    }
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String root(){
+        return "ok";
+    }
+
+
+
+}
---
+++ Demos/RabbitMqDemo2/src/main/java/com/woniuxy/RabbitMqDemo2Main.java
@@ -0,0 +1,14 @@
+package com.woniuxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqDemo2Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(RabbitMqDemo2Main.class);
+    }
+
+}
