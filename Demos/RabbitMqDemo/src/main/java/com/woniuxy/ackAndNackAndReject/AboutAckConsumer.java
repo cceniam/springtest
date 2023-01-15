fb3090390585cd75f553ef717383249e291da8b4
woniumrwang <woniumrwang@qq.com>
2023-01-15 16:08:07 +0800

    ack补充:  否定Ack时如果指定requeue为true但只有一个消费者时会出现无限循环

--- Demos/RabbitMqDemo/src/main/java/com/woniuxy/ackAndNackAndReject/AboutAckConsumer.java
+++ Demos/RabbitMqDemo/src/main/java/com/woniuxy/ackAndNackAndReject/AboutAckConsumer.java
@@ -18,7 +18,20 @@ public class AboutAckConsumer {
 
                 System.out.println("AboutAckConsumer 接收到一条消息: "+new String(body));
 
-                channel.basicAck(envelope.getDeliveryTag(),false);
+//                channel.basicAck(envelope.getDeliveryTag(),false);    //正常Ack,消息被消费掉,将从queue中移除
+
+                //Nack 否定ack ,同时这里指定了requeue为true
+                //此时RabbitMQ就会找其它消费者来处理消息
+                //如果消费者只有一个,那就会导致无限循环
+                channel.basicNack(envelope.getDeliveryTag(),false,true);
+
+                /*
+                 * Nack与Reject:
+                 *  Nack可以通过指定multiple为true来批量否定ack
+                 *  Reject只能单条否定ack, 等效于 channel.basicNack(envelope.getDeliveryTag(),false,true);
+                 */
+                //channel.basicReject(envelope.getDeliveryTag(),true);
+
             }
 
         });
