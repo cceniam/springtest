b17fd00a4f1375c64f79a31aa2c1dc70a2ff1b21
q2893 <cceniam@gmail.com>
2023-02-04 16:19:58 +0800

    开启缓存 开启rabbitmq

--- src/main/java/com/ch/libdemo/LibdemoApplication.java
+++ src/main/java/com/ch/libdemo/LibdemoApplication.java
@@ -1,8 +1,12 @@
 package com.ch.libdemo;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
+@EnableRabbit
 @SpringBootApplication
 public class LibdemoApplication {
 
